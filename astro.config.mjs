ff885c1d47badb5025ed14e5c6513e8a1062c748
Benjamin Arfa <benjamin.arfa.pro@gmail.com>
2025-01-20 13:50:47 +0100

    >fix: 2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "cnom-poc-organization.github.io",
+    site: "cnom-poc-organization.github.io",
+    base: "cnom-poc-organization.github.io",
 });
